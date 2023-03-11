a8916e5430a2fc82f2e8776b5ccc133a4dc1e781
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-11 13:43:49 +0200

    create EmployeesController and getAllEmployees

---
+++ lib/controller.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:data_caching/data/Employee.dart';
+import 'package:dio/dio.dart';
+import 'package:dio_http_cache/dio_http_cache.dart';
+
+class EmployeesController {
+  static String url = "https://dummy.restapiexample.com/api/v1/employees";
+  static List<Data> result = [];
+
+  static Future<List<Data>?> getAllEmployees() async {
+    try {
+      Dio dio = Dio();
+      DioCacheManager dioCacheManager = DioCacheManager(CacheConfig());
+      Options myOption =
+          buildCacheOptions(const Duration(days: 30), forceRefresh: true);
+      dio.interceptors.add(dioCacheManager.interceptor);
+
+      var response = await dio.get(url, options: myOption);
+
+      result = getList(response.data);
+    } catch (e) {
+      if (e is SocketException) {
+        return null;
+      }
+    }
+  }
+}
+
+getList(body) {
+  List<Data> emp = [];
+  List x = (body)['data'];
+  x.forEach((element) {
+    emp.add(Data.fromJson(element));
+  });
+
+  return emp;
+}
--- lib/presentation/home_view.dart
+++ lib/presentation/home_view.dart
@@ -5,6 +5,15 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Caching"),
+      ),
+      body: FutureBuilder(
+        future: ,
+        builder: (context, AsyncSnapshot snapshot) {
+          return ;
+        },),
+    );
   }
 }
